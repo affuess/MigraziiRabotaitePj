54ded9f42e619bdca135428bd285ecfe481b01ac
affuess <affuess@gmail.com>
2026-03-25 15:18:12 +0200

    token

--- Services/ITokenService.cs
+++ Services/ITokenService.cs
@@ -5,5 +5,7 @@ namespace MigraziiRabotaitePj.Services
     public interface ITokenService
     {
         Task<string> CreateAccessTokenAsync(AppUser user);
+        Task<string> CreateRefreshTokenAsync(AppUser user);
+        Task<bool> RevokeRefreshTokenAsync(string token);
     }
 }
--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using MigraziiRabotaitePj.Auth;
+using MigraziiRabotaitePj.Data;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -11,12 +12,15 @@ namespace MigraziiRabotaitePj.Services
     {
         private readonly IConfiguration _configuration;
         private readonly UserManager<AppUser> _userManager;
+        private readonly DataBase _context;
 
         public TokenService(IConfiguration configuration,
-            UserManager<AppUser> userManager)
+            UserManager<AppUser> userManager,
+            DataBase context)
         {
             _configuration = configuration;
             _userManager = userManager;
+            _context = context;
         }
 
         public async Task<string> CreateAccessTokenAsync(AppUser user)
@@ -49,6 +53,33 @@ namespace MigraziiRabotaitePj.Services
                 signingCredentials: credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public async Task<string> CreateRefreshTokenAsync(AppUser user)
+        {
+            var refreshToken = new RefreshToken
+            {
+                Id = Guid.NewGuid(),
+                Token = Guid.NewGuid().ToString("N"),
+                ExpireAtUtc = DateTime.UtcNow.AddDays(7),
+                UserId = user.Id,
+                User = user
+            };
+
+            _context.RefreshTokens.Add(refreshToken);
+            await _context.SaveChangesAsync();
+
+            return refreshToken.Token;
+        }
+
+        public async Task<bool> RevokeRefreshTokenAsync(string token)
+        {
+            var refreshToken = _context.RefreshTokens.FirstOrDefault(x => x.Token == token);
+            if (refreshToken == null) return false;
+
+            refreshToken.IsRevoke = true;
+            await _context.SaveChangesAsync();
+            return true;
         }
     }
 }
